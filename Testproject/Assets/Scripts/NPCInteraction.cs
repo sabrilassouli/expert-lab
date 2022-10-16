d04bfe9d0d8688e099b19e6d66f0b309bb430c93
sabrilassouli <sabri.lassouli@student.ehb.be>
2022-10-16 18:39:57 +0200

    Update NPCInteraction.cs
    now actuvate dialogue via collider

--- Testproject/Assets/Scripts/NPCInteraction.cs
+++ Testproject/Assets/Scripts/NPCInteraction.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class NPCInteraction : MonoBehaviour
 {
     [SerializeField] private GameObject startDialogue = null;
+    [SerializeField] private GameObject startUnhappyDialogue = null;
+    [SerializeField] private GameObject talkSign = null;
 
     // Update is called once per frame
     void Update()
@@ -17,5 +19,36 @@ public class NPCInteraction : MonoBehaviour
             startDialogue.SetActive(true);
         }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            talkSign.SetActive(true);
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("F key pressed");
+                Cursor.lockState = CursorLockMode.None;
+                //MouseLook.   find a way to switch bool indialogue to off in mouselook script to stop looking around 
+                startDialogue.SetActive(true);
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            talkSign.SetActive(false);
+            startDialogue.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
 
 }
